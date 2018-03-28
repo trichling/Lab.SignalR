efe3577ada41b6c562594efc7788813fb1a73ce9
Tobias Richling <trichling@gmx.de>
2018-03-28 18:27:44 +0200

    augmented vue instance

--- Lab.SignalR.Server/Views/Machines/Index.ts
+++ Lab.SignalR.Server/Views/Machines/Index.ts
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "vue-easy-toast/types/index";
 import Vue from "vue";
 import Toast  from "vue-easy-toast";
 import MachinesList from "./MachinesList";
